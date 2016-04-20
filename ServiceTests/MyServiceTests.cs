ed3d318645b5f0fc56155df0770285d327108233
JKHeadley <headley.justin@gmail.com>
2016-04-20 01:09:10 -0700

    Updated service test.

--- ServiceTests/MyServiceTests.cs
+++ ServiceTests/MyServiceTests.cs
@@ -12,6 +12,10 @@ namespace ServiceTests
         {
             using (var svc = new ServiceClient())
             {
+                //NOTE: Running this test twice will result in a duplicate error since two users can't have the same Id. This error
+                //will be logged and can viewed in the LogEvent table as an error example.
+                UserDTO rootUser = new UserDTO { Username = "rootUser", Id = Guid.Parse("86f0f79c-3ae9-4393-a437-f691f408135f") };
+                svc.CreateUser(rootUser);
                 OrganizationDTO org = new OrganizationDTO { Name = "MyOrg" };
                 var createdOrg = svc.CreateOrganization(org);
                 createdOrg.Name = "BobsOrg";
